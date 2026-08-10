91501ab2bb8416b9de4cac3dcc682401fc4a92c3
sidprs <sidprasad2004@gmail.com>
2026-08-10 16:38:33 -0700

    [dev] rework

---
+++ boot/boot.s
@@ -0,0 +1,27 @@
+; i686-elf-as boot.s -o boot.o
+.set MAGIC, 0x1BADB002
+.set FLAGS, 0x00000003
+
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+
+.section .multiboot
+.align 4
+.long MAGIC
+.long FLAGS
+.long CHECKSUM
+
+
+.section .bss
+.align 16
+stack_bottom: .skip 16384
+stack_top:
+
+.section .text
+.global _start
+_start:
+  mov $stack_top, %esp
+  call kernel_main
+  cli
+1: hlt
+  jmp 1b
