f1fb71bbb63eeeb41c03881dde8476fa1fece03b
sidprs <sidprasad2004@gmail.com>
2026-08-13 16:19:30 -0700

    [dev] kernel is unix based tyshi

--- boot/mem.hpp
+++ boot/mem.hpp
@@ -3,19 +3,7 @@
 #pragma once
 #include "types.hpp"
 // memcpy notes
-namespace arena {
 
-static u8 heap[64 * 1024];
-static u8* cursor = heap;
-
-void* arena_kmalloc(usize size, usize align = 8) {
-  usize p = ((usize)cursor + (align - 1)) & -(align - 1);
-  if (p + size > (usize)heap + sizeof(heap)) return (void*)code;
-  cursor = (u8*)(p + size);
-  return (void*)p;
-}
-
-}  // namespace arena
 extern "C" void* memcpy(void* dst, const void* src, usize len) {
   auto* d = (u8*)dst;
   auto* s = (const u8*)src;
@@ -29,5 +17,24 @@ extern "C" void* memset(void* dst, int c, usize n) {
   for (usize i = 0; i < n; ++i) d[i] = (u8)c;
   return dst;
 }
+
+namespace arena {
+
+static u8 heap[64 * 1024];
+static u8* cursor = heap;
+
+void* arena_kmalloc(usize size, usize align = 8) {
+  usize p = ((usize)cursor + (align - 1)) & ~(align - 1);
+  if (p + size > (usize)heap + sizeof(heap)) return (void*)code;
+  cursor = (u8*)(p + size);
+  return (void*)p;
+}
+
+inline void reset() {
+  memset(heap, 0, cursor - heap);
+  cursor = heap;
+}
+
+}  // namespace arena
 
 // arena allocater
